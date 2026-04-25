e527a149bd18836b72ca25181b3b9d1466669f72
kmusumba42-png <kmusumba42@gmail.com>
2026-04-25 13:53:51 +0000

    ok

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,41 +1,27 @@
 import streamlit as st
 from datetime import datetime
-
-# CONFIGURATION
 st.set_page_config(page_title="KIMS PRÉDICTION")
-
-# IDENTITÉ
 st.title("⚽ KIMS PRÉDICTION")
 st.write("### Expertise de **KimsFélicité**")
 st.sidebar.info("Partenaire : Félicité KILOLO MUSUMBA")
-
-# MENU SIMPLE
-choix = st.selectbox("Menu", ["VALUE BETS", "COUPONS", "PAYEMENT", "ADMIN"])
-
-# 1. VALUE BETS
+choix = st.selectbox("Menu",["VALUE BETS","COUPONS","PAYEMENT","ADMIN"])
 if choix == "VALUE BETS":
- st.header("Analyse des Matchs")
-  st.date_input("Choisir une date", datetime.now())
-   if st.button("Lancer l'Analyse"):
-     st.info("Recherche de Value Bets en cours...")
-
-     # 2. COUPONS
+ st.header("Analyse")
+  st.date_input("Date",datetime.now())
+   if st.button("Lancer"):
+     st.info("Recherche...")
      if choix == "COUPONS":
       st.header("Espace Coupons")
-       st.success("🎫 COUPON GRATUIT : Disponible")
-        st.warning("🎫 COUPON PREMIUM : Réservé aux VIP")
-
-        # 3. PAYEMENT
+       st.success("🎫 COUPON GRATUIT disponible")
+        st.warning("🎫 COUPON PREMIUM : VIP")
         if choix == "PAYEMENT":
-         st.header("Abonnements Premium")
+         st.header("Abonnements")
           st.write("M-Pesa : +243 816 272 212")
            st.write("Airtel : +243 996 660 000")
             st.write("Orange : +243 855 415 399")
-             st.write("Tarifs : 3000 FC (Semaine) | 12000 FC (Mois)")
-
-             # 4. ADMIN
-             if choix == "ADMIN":
-              st.header("Contrôle Partenaire")
-               psw = st.text_input("Code Secret", type="password")
-                if psw == "243":
-                  st.success("### Bienvenue, Partenaire Félicité KILOLO MUSUMBA")
+            if choix == "ADMIN":
+             st.header("Contrôle")
+              psw = st.text_input("Code",type="password")
+               if psw == "243":
+                 st.success("Bienvenue, Partenaire")
+                 
